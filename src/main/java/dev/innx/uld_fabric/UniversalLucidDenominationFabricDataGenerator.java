3fa369747f96569643909387e1ca8b65b3974ae6
Kazuhiko-Gushiken <just.a.stem.nerd@gmail.com>
2024-03-14 21:34:55 -0700

    Initial commit

---
+++ src/main/java/dev/innx/uld_fabric/UniversalLucidDenominationFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.innx.uld_fabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UniversalLucidDenominationFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
